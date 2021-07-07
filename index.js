8e9e6188422df8b8238a0b6b108fcf96ea46e756
thao15059 <thao15059@gmail.com>
2021-07-07 23:24:39 +0700

    initial project

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const app = express();
+const port = 3003;
+
+const server = app.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
+});
+
+app.set("view engine", "pug");
+app.set("views", "views");
+
+app.get("/", (req, res, next) => {
+  const payload = {
+    pageTitle: "Home",
+  };
+
+  res.status(200).render("home", payload);
+});
