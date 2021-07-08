dcaff906c78462ccc319db77da1e5c9a8ef94817
thao15059 <thao15059@gmail.com>
2021-07-09 00:15:24 +0700

    implements login views, login routes, login css

--- index.js
+++ index.js
@@ -1,15 +1,27 @@
 const express = require("express");
+const path = require("path");
+
+const middleware = require("./middleware");
+
 const app = express();
 const port = 3003;
 
-const server = app.listen(port, () => {
+app.listen(port, () => {
   console.log(`Server listening on port ${port}`);
 });
 
 app.set("view engine", "pug");
 app.set("views", "views");
 
-app.get("/", (req, res, next) => {
+// Statics
+app.use(express.static(path.join(__dirname, "public")));
+
+// Routes
+const loginRoute = require("./routes/loginRoutes");
+
+app.use("/login", loginRoute);
+
+app.get("/", middleware.requireLogin, (req, res, next) => {
   const payload = {
     pageTitle: "Home",
   };
